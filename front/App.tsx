42ae99e3450c7d7205003ab7a8dbc78a506033b5
sAmH365 <npower5377@naver.com>
2024-04-06 11:20:16 +0900

    flexbox

--- front/App.tsx
+++ front/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import {
-  Button, SafeAreaView,
-  StyleSheet, Text, TextInput,
-  View,
-} from 'react-native';
-
+import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} />
         <Text>텍스트</Text>
-        <Button title="버튼이름" onPress={() => console.log('클릭!!')}></Button>
-        <TextInput />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} />
+        <Text>텍스트</Text>
       </View>
     </SafeAreaView>
   );
@@ -20,11 +18,26 @@ function App(): React.JSX.Element {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     marginTop: 50,
-    backgroundColor: 'red',
+    backgroundColor: 'yellow',
     margin: '10%',
     marginHorizontal: 10,
     marginVertical: 560,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: 'black',
+    height: 100,
+    width: 100,
+  },
+  inputContainer: {
+    flex: 1,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'center',
+    // justifyContent: 'space-evenly',
   },
 });
 
